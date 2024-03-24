7df3dc85ae4f8bb56df35648d0f4788c411b0d72
Anik <64964667+anik-75@users.noreply.github.com>
2024-03-24 23:50:08 +0530

    headings

---
+++ src/components/main/Heading.jsx
@@ -0,0 +1,19 @@
+import "./Heading.css";
+const Heading = () => {
+  return (
+    <>
+      <main className="main">
+        <div className="headings-container">
+          <div className="heading-title-container">
+            <h1>Featured Products</h1>
+          </div>
+          <div className="subheading-container">
+            <p>Explore and discover a variety of products</p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Heading;
